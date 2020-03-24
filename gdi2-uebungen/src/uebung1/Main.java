1d46f061334b1600749807a84ce32d029573c53a
jahtzee <jahtzee@ARRAKIS>
2020-03-24 17:13:04 +0100

    add more test conditions

--- gdi2-uebungen/src/uebung1/Main.java
+++ gdi2-uebungen/src/uebung1/Main.java
@@ -5,13 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Vehicle horstCar = new Car("Jeep Cherokee", "7MIE858", 9);
-		Vehicle horstBike = new Ebike("R Raymon Tourray E", 23);
+		Vehicle horstBike = new Ebike("R Raymon Tourray E", -5000);
 		Vehicle[] horstVehicles = {horstCar, horstBike};
 		Person horst = new Person("Horst Müller", horstVehicles, 2);
 		
 		System.out.println(horst.buyVehicle(horstCar));
-		System.out.println(horst.buyVehicle(horstBike));
+		//System.out.println(horst.buyVehicle(horstBike));
 		horst.printVehicles();
+		horstCar.checkRange();
+		horstBike.checkRange();
 	}
 
 }
