86533c60d45ea0c03f18384c51970708cf543473
jahtzee <jahtzee@ARRAKIS>
2020-04-20 12:16:48 +0200

    add more efficient limits

--- gdi2-uebungen/src/uebung3/petshop.java
+++ gdi2-uebungen/src/uebung3/petshop.java
@@ -3,7 +3,7 @@ package uebung3;
 public class petshop {
 
 	public static void main(String[] args) {
-		int value = 100;
+		int value = 120;
 		int centValue = value*100;
 		
 		int mouseValue = 50;
@@ -13,8 +13,8 @@ public class petshop {
 		int result;
 		
 		for (int x = 1; x < 99; x++) {
-			for (int y = 1; y < 99; y++) {
-				for (int z = 1; z < 99; z++) {
+			for (int y = 1; y < (centValue/hamsterValue) && x+y <= 100; y++) {
+				for (int z = 1; z < (centValue/turtleValue) && x+y+z <= 100; z++) {
 					result = (mouseValue*x)+(hamsterValue*y)+(turtleValue*z);
 					if (result == centValue && x+y+z == 100) {
 						System.out.print("mice: " + x);
